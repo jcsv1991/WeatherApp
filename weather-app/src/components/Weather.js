d01859be495a09507263a954182fb6e4eb5f5b33
jcsv1991 <166552973+jcsv1991@users.noreply.github.com>
2024-10-17 09:37:35 -0700

    set up

---
+++ weather-app/src/components/Weather.js
@@ -0,0 +1,12 @@
+// src/components/Weather.js
+import React from 'react';
+
+const Weather = () => {
+  return (
+    <div>
+      <h1>Weather Forecast</h1>
+    </div>
+  );
+};
+
+export default Weather;
