acd876c8587f7f8f6aa0022eb97653b8a577320b
jcsv1991 <166552973+jcsv1991@users.noreply.github.com>
2024-10-18 12:05:59 -0700

    FinalVersion

--- weather-app/src/components/WeatherDisplay.js
+++ weather-app/src/components/WeatherDisplay.js
@@ -1,6 +1,6 @@
 // src/components/WeatherDisplay.js
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Card, CardContent } from '@mui/material';
 
 const WeatherDisplay = ({ weather, city, countryCode, units }) => {
   if (!weather) return null;
@@ -8,18 +8,23 @@ const WeatherDisplay = ({ weather, city, countryCode, units }) => {
   const tempUnit = units === 'metric' ? '°C' : '°F';
 
   return (
-    <Box mt={3} textAlign="center">
-      <Typography variant="h4">
-        {city}, {countryCode}
-      </Typography>
-      <Typography variant="h6">{weather.weather[0].description}</Typography>
-      <Typography variant="h5">
-        Temperature: {weather.main.temp}{tempUnit}
-      </Typography>
-      <img
-        src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
-        alt="weather icon"
-      />
+    <Box mt={3} textAlign="center" display="flex" justifyContent="center">
+      <Card sx={{ maxWidth: 345, textAlign: 'center' }}> {/* Card around the content */}
+        <CardContent>
+          <Typography variant="h4">
+            {city}, {countryCode}
+          </Typography>
+          <Typography variant="h6">{weather.weather[0].description}</Typography>
+          <Typography variant="h5">
+            Temperature: {weather.main.temp}{tempUnit}
+          </Typography>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+            alt="weather icon"
+            style={{ marginTop: 16 }}
+          />
+        </CardContent>
+      </Card>
     </Box>
   );
 };
